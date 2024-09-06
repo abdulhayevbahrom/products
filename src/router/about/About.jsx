fc5be8a33ef2ca5bd2776e82793d1f4a8272edb9
abdulhayevbahrom <abdulhayevbahromjon2002@gmail.com>
2024-09-06 09:14:30 +0500

    chabge about

--- src/router/about/About.jsx
+++ src/router/about/About.jsx
@@ -4,6 +4,20 @@ function About() {
   return (
     <div className="about">
       <h1>About page</h1>
+      <main>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        tenetur ex eos, enim eveniet doloremque itaque ab impedit fugit ipsa
+        voluptate in natus dolore consectetur cupiditate consequatur eaque
+        maxime nobis veniam voluptatem, deleniti aspernatur eligendi quam. Quod
+        nihil neque modi non, quo perferendis quasi at, numquam repellendus,
+        esse omnis explicabo impedit blanditiis ducimus molestias perspiciatis
+        cupiditate? Quae cupiditate veritatis in voluptates? Est commodi vel
+        voluptates, nobis quos reiciendis neque rem perspiciatis natus ipsum
+        dolorem, vitae dolor officiis impedit. Dolore, assumenda vitae laborum
+        quisquam temporibus maiores animi? Reiciendis beatae, hic molestias,
+        commodi corporis deleniti pariatur voluptates officiis sit odio aut?
+        Quas.
+      </main>
     </div>
   );
 }
